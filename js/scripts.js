9f8de34122c31745a6a4cdf411b221f0075dab37
Andy Tuttle <andytuttle12@gmail.com>
2016-12-14 11:01:22 -0500

    new try on JS and css styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,102 +1,69 @@
+// ----------GLOBALS----------
+var theDeck = createDeck();
+var playersHand = [];
+var dealersHand = [];
+// ---------------------------
+
 $(document).ready(function(){
 
-	// ----------GLOBALS----------
-
-	var theDeck = [];
-	var playersHand = [];
-	var dealersHand = [];
-	var dealerTotal = 0;
-	var playerTotal = 0;
-
-	// ---------------------------
-
-	createDeck();
-
+	// Major Buttons for the game
 	$('.deal-button').click(function(){
-		// console.log(this);
-		shuffleDeck();
-		// Add card to playersHand
+		shuffleDeck(); // now shuffled!
 		playersHand.push(theDeck[0]);
-		// Add to dealer as well
-		dealersHand.push(theDeck[1]);
-		// to player
 		playersHand.push(theDeck[2]);
-		// to dealer (face down)
-		dealersHand.push(theDeck[3]);
-		// put the first card in the player's hand
-		placeCard(playersHand[0], 'player', 'one');
-		placeCard(playersHand[1], 'player', 'two');
-		// put the cards in the dealer's hand
-		placeCard(dealersHand[0], 'dealer', 'one');
-		placeCard(dealersHand[1], 'dealer', 'two');
 
-		calculateTotal('player',playersHand);
-		calculateTotal('dealer',dealersHand);
-	})
+		placeCard('player','one',playersHand[0]);
+		placeCard('player','two',playersHand[1]);
+
+		dealersHand.push(theDeck[1]);
+		dealersHand.push(theDeck[3]);
+
+		placeCard('dealer','one',dealersHand[0]);
+		placeCard('dealer','two',dealersHand[1]);
+	});
 
 	$('.hit-button').click(function(){
-		// console.log(this);
-	})
+
+	});
+
 	$('.stand-button').click(function(){
-		// console.log(this);
-	})
+
+	});
+
 	$('.double-button').click(function(){
-		// console.log(this);
-	})
 
-	function createDeck(){
-		// fill with -52 cards -4 suits -1-13 (1-10,J,Q,K)
-		var suits = ['h','s','d','c'];
-		// loop through all four suits
-		for (let s = 0; s < suits.length; s++){
-			// loop through all 13 cards
-			for(let c = 1; c <= 13; c++){
-				theDeck.push(c + suits[s]);
+	});
 
-			}
-		}
-		
-	}
+	$('.split-button').click(function(){
 
-	function shuffleDeck(){
-		// randomize the array
-		for(let i = 0; i < 9001; i++){
-			var card1ToSwitch = Math.floor(Math.random() * theDeck.length);
-			var card2ToSwitch = Math.floor(Math.random() * theDeck.length);
-			var temp = theDeck[card1ToSwitch];
-			theDeck[card1ToSwitch] = theDeck[card2ToSwitch];
-			theDeck[card2ToSwitch] = temp;
-		}
-	}
+	});
 
-	function placeCard(whatCard, who, whichSlot){
-		var classToTarget = '.' + who + '-cards .card-' + whichSlot;
-		// console.log(classToTarget);
-		$(classToTarget).html('<img src="images/' + whatCard + '.png">');
-		if((classToTarget == '.dealer-cards .card-'+ whichSlot) && (whichSlot == 'two')){
-			$(classToTarget).html('<img src="images/deck.png">');
-			$('.dealer-total-number').hide();
-			$('.deal-button').attr('disabled', 'disabled');
-		}
-	}
 
-	function calculateTotal(who, theirHand){
-		var cardValue = 0;
-		var total = 0;
-		for(let i = 0; i < theirHand.length; i++){
-			cardValue = Number(theirHand[i].slice(0, -1));
-			// console.log(cardValue);
-			if(cardValue > 10){
-				cardValue = 10;
-			}
-			if(cardValue === 1 && total <= 10){
-				cardValue = 11;
-			}
-			total += cardValue;
-
-		}
-		var classToTarget = '.'+who+'-total-number';
-		$(classToTarget).text(total);
-	}console.log(playerTotal);
 });
 
+function createDeck(){
+	var newDeck = [];
+	var suits = ['h','s','d','c'];
+	for(let s = 0; s < suits.length; s++){
+		for(let c = 1; c <= 13; c++){
+			newDeck.push(c+suits[s]);
+		}
+	}
+	return newDeck;
+}
+
+function shuffleDeck(){
+	for(let i = 0; i < 9001; i++){
+		var random1 = Math.floor(Math.random()*theDeck.length);
+		var random2 = Math.floor(Math.random()*theDeck.length);
+		var temp = theDeck[random1];
+		theDeck[random1] = theDeck[random2];
+		theDeck[random2] = temp;
+	}
+	console.log(theDeck);
+}
+
+function placeCard(who, where, whatCard){
+	var classSelector = '.' + who + '-cards .card-' + where;
+	$(classSelector).html('<img src="images/' + whatCard + '.png">');
+}
