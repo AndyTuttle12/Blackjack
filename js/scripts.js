475a33a37b3da48cf14d09fa6bc453bcd8a7f247
Andy Tuttle <andytuttle12@gmail.com>
2016-12-14 20:32:11 -0500

    added more styles, buttons, placements, and started animations

--- js/scripts.js
+++ js/scripts.js
@@ -2,32 +2,67 @@
 var theDeck = createDeck();
 var playersHand = [];
 var dealersHand = [];
+var topOfDeck = 4;
+var hiddenDealerCard;
 // ---------------------------
 
 $(document).ready(function(){
 
+	// console.log("freshDeck on page - "+freshDeck);
+	$('.hit-button').attr('disabled', 'disabled');
+	$('.stand-button').attr('disabled', 'disabled');
+	$('.double-button').attr('disabled', 'disabled');
+	$('.split-button').hide();
+	$('.reset-button').hide();
 	// Major Buttons for the game
 	$('.deal-button').click(function(){
 		shuffleDeck(); // now shuffled!
-		playersHand.push(theDeck[0]);
-		playersHand.push(theDeck[2]);
+		playersHand.push(theDeck.shift());
+		dealersHand.push(theDeck.shift());
+		playersHand.push(theDeck.shift());
+		dealersHand.push(theDeck.shift());
+		// console.log("freshDeck after shifts - ", freshDeck);
 
-		placeCard('player','one',playersHand[0]);
-		placeCard('player','two',playersHand[1]);
+		placeCard('player',1,playersHand[0]);
+		placeCard('player',2,playersHand[1]);
 
-		dealersHand.push(theDeck[1]);
-		dealersHand.push(theDeck[3]);
 
-		placeCard('dealer','one',dealersHand[0]);
-		placeCard('dealer','two',dealersHand[1]);
+		placeCard('dealer',1,dealersHand[0]);
+		placeCard('dealer',2,dealersHand[1]);
+		hiddenDealerCard = dealersHand[1];
+		calculateTotal(playersHand, 'player');
+		calculateTotal(dealersHand, 'dealer');
+
+		$('.deal-button').attr('disabled', 'disabled');
+		$('.hit-button').removeAttr('disabled', 'disabled');
+		$('.stand-button').removeAttr('disabled', 'disabled');
+		$('.double-button').removeAttr('disabled', 'disabled');
 	});
 
 	$('.hit-button').click(function(){
-
+		if(calculateTotal(playersHand,'player') <= 21){
+			// add a card to js and document; update total
+			$('.double-button').attr('disabled', 'disabled');
+			playersHand.push(theDeck.shift());
+			var slotForNewCard = playersHand.length;
+			placeCard('player',slotForNewCard,playersHand[playersHand.length-1]);
+			calculateTotal(playersHand, 'player');
+		}
 	});
 
 	$('.stand-button').click(function(){
+		var dealerTotal = calculateTotal(dealersHand,'dealer');
+		$('.dealer-cards .card-2').html('<img src="images/' + hiddenDealerCard + '.png">');
+		while(dealerTotal < 17){
+			dealersHand.push(theDeck.shift());
+			var slotForNewCard = dealersHand.length;
+			placeCard('dealer',slotForNewCard,dealersHand[dealersHand.length-1]);
+			dealerTotal = calculateTotal(dealersHand, 'dealer');
+		}
+		$('.stand-button').attr('disabled', 'disabled');
+		$('.dealer-total-number').show();
 
+		checkWin();
 	});
 
 	$('.double-button').click(function(){
@@ -38,8 +73,49 @@ $(document).ready(function(){
 
 	});
 
+	$('.reset-button').click(function(){
+		reset();
+		$('.reset-button').hide();
+	});
+
 
 });
+
+function checkWin(){
+	playerTotal = calculateTotal(playersHand,'player');
+	dealerTotal = calculateTotal(dealersHand,'dealer');
+
+	if(playerTotal > 21){
+		// Player busted.
+	}else if(dealerTotal > 21){
+		// Dealer busted.
+	}else{
+		if(playerTotal > dealerTotal){
+			// player won.
+		}else if(dealerTotal > playerTotal){
+			// Dealer won.
+		}else{
+			// push...no winner...default to dealer win.
+		}
+	}
+	$('.reset-button').show();
+}
+
+
+function reset(){
+	// reset hands and deck...and the DOM
+	theDeck = createDeck();
+	playersHand = [];
+	dealersHand = [];
+	$('.card').html('');
+	// console.log(freshDeck);
+
+
+	// console.log(theDeck);
+	playerTotal = calculateTotal(playersHand,'player');
+	dealerTotal = calculateTotal(dealersHand,'dealer');
+	$('.deal-button').removeAttr('disabled', 'disabled');
+}
 
 function createDeck(){
 	var newDeck = [];
@@ -60,10 +136,51 @@ function shuffleDeck(){
 		theDeck[random1] = theDeck[random2];
 		theDeck[random2] = temp;
 	}
-	console.log(theDeck);
+	// console.log(theDeck);
 }
 
 function placeCard(who, where, whatCard){
 	var classSelector = '.' + who + '-cards .card-' + where;
 	$(classSelector).html('<img src="images/' + whatCard + '.png">');
+	if(classSelector == '.dealer-cards .card-2'){
+		$(classSelector).html('<img src="images/deck.png">');
+
+		$('.dealer-total-number').hide();
+		$('.deal-button').attr('disabled', 'disabled');
+	}
 }
+
+function calculateTotal(hand, who){
+	var total = 0; //running total
+	var cardValue = 0; //temp value of card
+	var countAce = 0; //ace counter
+	for(let i = 0; i < hand.length; i++){
+		cardValue = Number(hand[i].slice(0,-1));
+		if(cardValue > 10){
+			cardValue = 10;
+		}
+		if(cardValue == 1){
+			countAce++;
+		}
+		if(cardValue === 1 && total <= 10){
+			cardValue = 11;
+		}
+		total += cardValue;
+
+		while((total > 21)&&(countAce !== 0)){
+            total -= 10;
+            countAce--;
+        }
+	}
+	var classSelector = '.' + who + '-total-number';
+	$(classSelector).text(total);
+	return total;
+}
+
+function arrangeDeck(){
+	var deckArray = [];
+	for(let i = 0; i < deckArray.length; i++){
+		deckArray[i]
+	}
+}
+
